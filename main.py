53f43177d6a5a1a13870d824029faaf26dad517c
kefkahacks <sgray@linuxmail.org>
2018-07-27 02:48:24 -0400

    defined nmap, updated banner, moved banner outside of main, cleanly returns to menu after invalid

--- main.py
+++ main.py
@@ -11,32 +11,42 @@ __copyright__ = "copyright 2018, GPl v3.0"
 import time
 import os
 import sys
+import subprocess
 from core import banner
+
+#banner before menu
+banner.test()
+time.sleep(3)
 
 def main():
 #displays number for different hacking tools
-    banner.test()
-    time.sleep(3)
-    print "select the values for the options of tools available\n"
-    print"1-metaspoilt"
-    print"2-nmap"
-    print"3-hulk"
-    print"4-Zmap"
+    print "BabyMux recommends the following tools:"
+    time.sleep(1)
+    print"1-metasploit - exploitation"
+    print"2-nmap - Discovery/OSINT/Vulnerability Analysis"
+    print"3-hulk - Stress teet"
+    print"4-Zmap - very fast scanner"
     print"5-scapy"
     print"6-subBrute-force"
-    print"7-sqlmap"
+    print"7-sqlmap - SQLi/XSS Discovery/Exploitation"
     print"8-xshell"
     print"9-redhawk"
-    print"10-routerspoilt"
-    print"11-hydra"
+    print"10-routersploit - exploitation"
+    print"11-hydra - password bruteforce"
     print"12-breacher"
     print"13-batch-download\n"
     print"00-exit mars"
 
-    babymux = raw_input("select your values>>")
+    babymux = raw_input("Select a tool to install>>")
+
+# make a global do something
+    def nmap():
+        print subprocess.Popen("apt-get install -y nmap", shell=True, stdout=subprocess.PIPE).stdout.read()
+        time.sleep(1)
+        main()
 
     if babymux =='1' or babymux == '01':
-        metaspoilt()
+        metasploit()
     elif babymux == '2' or babymux == '02':
         nmap()
     elif babymux == '3' or babymux == '03':
@@ -54,7 +64,7 @@ def main():
     elif babymux == '10':
         red-hawk()
     elif babymux == '11':
-        routerspoilt()
+        routersploit()
     elif babymux == '12':
         hydra()
     elif babymux == '13':
@@ -66,7 +76,7 @@ def main():
     else:
         print "\n please enter a valid input"
         time.sleep(3)
-        restart()
+        return main()
 
 if __name__ == "__main__":
 	main()
