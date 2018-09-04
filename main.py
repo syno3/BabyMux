b0c8cb3babfdcc8e7bf2e97b973dfad0f7cd4881
Festus murimi <sinotechxvista@gmail.com>
2018-09-04 11:12:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ import sys
 import subprocess
 from core import banner
 from core import core
+#terminal colors
+
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
 
 #banner before menu
 banner.test()
@@ -28,20 +38,20 @@ def main():
 #displays number for different hacking tools
     print "BabyMux recommends the following tools:"
     time.sleep(1)
-    print"1-metasploit - exploitation"
-    print"2-nmap - Discovery/OSINT/Vulnerability Analysis"
-    print"3-hulk - Stress teet"
-    print"4-Zmap - very fast scanner"
-    print"5-scapy"
-    print"6-subBrute-force"
-    print"7-sqlmap - SQLi/XSS Discovery/Exploitation"
-    print"8-xshell"
-    print"9-redhawk"
-    print"10-routersploit - exploitation"
-    print"11-hydra - password bruteforce"
-    print"12-breacher"
-    print"13-batch-download\n"
-    print"00-exit babyMux"
+    print"WARNING+1-metasploit - exploitation"
+    print"WARNING+2-nmap - Discovery/OSINT/Vulnerability Analysis"
+    print"WARNING+3-hulk - Stress teet"
+    print"WARNING+4-Zmap - very fast scanner"
+    print"WARNING+5-scapy"
+    print"WARNING+6-subBrute-force"
+    print"WARNING+7-sqlmap - SQLi/XSS Discovery/Exploitation"
+    print"WARNING+8-xshell"
+    print"WARNING+9-redhawk"
+    print"WARNING+10-routersploit - exploitation"
+    print"WARNING+11-hydra - password bruteforce"
+    print"WARNING+12-breacher"
+    print"WARNING+13-batch-download\n"
+    print"WARNING+00-exit babyMux"
 
     babymux = raw_input("Select a tool to install>>")
  
